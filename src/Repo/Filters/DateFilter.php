9d1e8279388e05e9219726b58d64ea10bf9ddc48
Alexander Shuleyko <salex009@gmail.com>
2018-09-21 18:27:02 +0300

    add date filter

---
+++ src/Repo/Filters/DateFilter.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace QuadStudio\Repo\Filters;
+
+use QuadStudio\Repo\Contracts\RepositoryInterface;
+use QuadStudio\Repo\Tag;
+
+abstract class DateFilter extends FormFilter
+{
+
+    /**
+     * @var string
+     */
+    protected $search = 'date';
+
+
+    function apply($builder, RepositoryInterface $repository)
+    {
+        if ($this->canTrack() && $this->filled($this->search)) {
+            $builder = $builder->where($this->column(), $this->get($this->search));
+        }
+
+        return $builder;
+    }
+
+    /**
+     * @return string
+     */
+    abstract function column();
+
+    /**
+     * @return Tag
+     */
+    public function tag(): Tag
+    {
+        if (is_null($this->tag)) {
+            $attributes = $this->attributes();
+            if ($this->has($this->name()) && $this->get($this->name()) != "") {
+                $attributes['value'] = $this->get($this->name());
+            }
+            $this->tag = new Tag('date', [
+                'attributes' => $attributes,
+            ]);
+        }
+
+        return $this->tag;
+    }
+
+    /**
+     * @return string
+     */
+    public function name(): string
+    {
+        return $this->search;
+    }
+
+    /**
+     * @return array
+     */
+    public function track()
+    {
+        return [
+            $this->search,
+        ];
+    }
+
+}
