a2b92927feaf0bd117e5b78374be9954773332f7
Alexander Shuleyko <salex009@gmail.com>
2019-09-06 19:57:24 +0300

    add setBuilder method

--- src/Repo/Eloquent/Repository.php
+++ src/Repo/Eloquent/Repository.php
@@ -15,370 +15,407 @@ use QuadStudio\Repo\Filters\FormFilter;
 
 abstract class Repository implements RepositoryInterface, Filterable
 {
-    /**
-     * @var Application
-     */
-    protected $app;
 
-    /**
-     * @var Model
-     */
-    protected $model;
+	/**
+	 * @var Application
+	 */
+	protected $app;
 
-    /**
-     * @var Collection
-     */
-    protected $filters;
+	/**
+	 * @var Model
+	 */
+	protected $model;
 
-    /**
-     * @var string
-     */
-    protected $table;
+	/**
+	 * @var Collection
+	 */
+	protected $filters;
 
-    /**
-     * @var bool
-     */
-    protected $isTracked = false;
+	/**
+	 * @var string
+	 */
+	protected $table;
 
-    /**
-     * @var bool
-     */
-    protected $skipFilter = false;
+	/**
+	 * @var bool
+	 */
+	protected $isTracked = false;
 
-    private $trackedFilters = [];
+	/**
+	 * @var bool
+	 */
+	protected $skipFilter = false;
 
-    /**
-     * @param Application $app
-     */
-    public function __construct(Application $app)
-    {
-        $this->app = $app;
-        $this->resetFilters();
-        $this->getModel();
-        //$this->makePresenter();
-        //$this->makeValidator();
+	private $trackedFilters = [];
 
-        $this->boot();
-    }
+	/**
+	 * @param Application $app
+	 */
+	public function __construct(Application $app)
+	{
+		$this->app = $app;
+		$this->resetFilters();
+		$this->getModel();
+		//$this->makePresenter();
+		//$this->makeValidator();
+		$this->boot();
+	}
 
-    /**
-     * Reset all Filters
-     *
-     * @return $this
-     */
-    public function resetFilters()
-    {
-        $this->filters = new Collection();
+	/**
+	 * Reset all Filters
+	 *
+	 * @return $this
+	 */
+	public function resetFilters()
+	{
+		$this->filters = new Collection();
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * @return Builder
-     * @throws RepositoryException
-     */
-    public function getModel()
-    {
-        if (!$this->app->has($this->model())) {
-            $this->app->bind($this->model());
-            $model = $this->app->make($this->model());
+	/**
+	 * @param \Illuminate\Database\Eloquent\Builder $builder
+	 *
+	 * @return $this
+	 */
+	public function setBuilder(Builder $builder){
+		$this->model = $builder;
+		return $this;
+	}
 
-            if (!$model instanceof Model) {
-                $this->table = $model->getTable();
-                throw new RepositoryException(trans('repo::exception.instance', ['class' => $this->model(), 'instance' => Model::class]));
-            }
-            $this->model = $model;
-        }
+	/**
+	 * @return Builder
+	 */
+	public function getModel()
+	{
+		if (!$this->app->has($this->model())) {
+			$this->app->bind($this->model());
+			$model = $this->app->make($this->model());
 
-        return $this->model;
-    }
+			try {
+				if (!$model instanceof Model) {
+					$this->table = $model->getTable();
 
-    /**
-     * Specify Model class name
-     *
-     * @return string
-     */
-    abstract public function model();
+					throw new RepositoryException(trans('repo::exception.instance', ['class' => $this->model(), 'instance' => Model::class]));
+				}
+			} catch (RepositoryException $exception) {
+				dd($exception->getMessage());
+			}
 
-    /**
-     *
-     */
-    public function boot()
-    {
-        //
-    }
+			$this->model = $model;
+		}
 
-    public function getTable()
-    {
-        return $this->table;
-    }
+		return $this->model;
+	}
 
-    /**
-     * @param bool $status
-     * @return $this
-     */
-    public function skipFilter($status = true)
-    {
-        $this->skipFilter = $status;
+	/**
+	 * Specify Model class name
+	 *
+	 * @return string
+	 */
+	abstract public function model();
 
-        return $this;
-    }
+	/**
+	 *
+	 */
+	public function boot()
+	{
+		//
+	}
 
-    /**
-     * Skip Filters
-     *
-     * @param bool $status
-     *
-     * @return $this
-     */
-    public function trackFilter($status = true)
-    {
-        $this->isTracked = $status;
+	public function getTable()
+	{
+		return $this->table;
+	}
 
-        return $this;
-    }
+	/**
+	 * @param bool $status
+	 *
+	 * @return $this
+	 */
+	public function skipFilter($status = true)
+	{
+		$this->skipFilter = $status;
 
-    /**
-     * @param array $columns
-     * @return mixed
-     */
-    public function all($columns = array('*'))
-    {
+		return $this;
+	}
 
-        $this->applyFilters();
+	/**
+	 * Skip Filters
+	 *
+	 * @param bool $status
+	 *
+	 * @return $this
+	 */
+	public function trackFilter($status = true)
+	{
+		$this->isTracked = $status;
 
+		return $this;
+	}
 
-        return $this->getModel()->get($columns);
-    }
+	/**
+	 * @param array $columns
+	 *
+	 * @return mixed
+	 */
+	public function all($columns = ['*'])
+	{
 
-    /**
-     * Apply filters
-     *
-     * @return $this
-     */
-    public function applyFilters()
-    {
-        if ($this->skipFilter === false) {
-
-            $this->pushTracked();
-
-            foreach ($this->getFilters() as $filter) {
-                $this->applyFilter($filter);
-            }
-
-        }
-
-        //dd($this->getFilters());
-
-        return $this;
-    }
-
-    /**
-     * @return $this
-     * @throws \Exception
-     */
-    public function pushTracked()
-    {
-        if ($this->isTracked === true) {
-            $filters = array_unique(array_merge($this->track(), $this->trackedFilters));
-            foreach ($filters as $filterClass) {
-                if (!($filter = $this->app->make($filterClass)) instanceof FilterInterface) {
-                    throw new \Exception(trans('repo::exception.instance', ['class' => $filterClass, 'instance' => FilterInterface::class]));
-                }
-                $this->pushFilter($filter);
-            }
-        }
-
-        return $this;
-    }
-
-    /**
-     * @return array
-     */
-    public function track(): array
-    {
-        return [];
-    }
-
-    /**
-     * @param FilterInterface $filter
-     * @return $this
-     */
-    public function pushFilter(FilterInterface $filter)
-    {
-        $this->filters->push($filter);
-
-        return $this;
-    }
-
-    /**
-     * @return Collection
-     */
-    public function getFilters()
-    {
-        return $this->filters;
-    }
-
-    /**
-     * @param FilterInterface $filter
-     * @return $this
-     */
-    public function applyFilter(FilterInterface $filter)
-    {
-        $this->model = $filter->apply($this->model, $this);
-
-        return $this;
-    }
-
-    public function toSql()
-    {
-        return $this->getModel()->toSql();
-    }
-
-    public function getBindings()
-    {
-        return $this->getModel()->getBindings();
-    }
-
-    public function orderBy(array $columns = [])
-    {
-        if (!empty($columns)) {
-
-            foreach ($columns as $column_name => $direction) {
-                if (is_int($column_name)) {
-                    $column_name = $direction;
-                    $direction = 'ASC';
-                }
-                $this->model = $this->getModel()->orderBy($column_name, $direction);
-            }
-        }
-
-        return $this;
-    }
-
-    /**
-     * @param $filter
-     * @return $this
-     */
-    public function pushTrackFilter($filter)
-    {
-        $this->trackedFilters[] = $filter;
-        return $this;
-    }
-
-    /**
-     * @param $offset
-     * @param $limit
-     * @return Collection
-     */
-    public function offset($offset, $limit, $columns = ['*'])
-    {
-        $this->applyFilters();
-
-        return $this->getModel()
-            ->select($columns)
-            ->offset($offset)
-            ->limit($limit)
-            ->get();
-    }
-
-    /**
-     * @param $chunk
-     * @param callable $callback
-     * @return mixed
-     */
-    public function chunk($chunk, callable $callback)
-    {
-        $this->applyFilters();
-
-        return $this->getModel()->chunk($chunk, $callback);
-    }
-
-    public function count()
-    {
-        $this->applyFilters();
-
-        return $this->getModel()->count();
-    }
-
-    /**
-     * @param int $perPage
-     * @param array $columns
-     * @return mixed
-     */
-    public function paginate($perPage = null, $columns = ['*'], $pageName = 'page', $page = null)
-    {
-        $this->applyFilters();
-        if (request()->has('sql')) {
-            //dump($this->getModel());
-            dump($this->getModel()->getBindings());
-            dd($this->getModel()->toSql());
-        }
+		$this->applyFilters();
 
 
+		return $this->getModel()->get($columns);
+	}
 
-        return $this->getModel()
-            ->paginate($perPage, $columns, $pageName, $page)
-            ->appends(request()->except(['page', '_token']));
-    }
+	/**
+	 * Apply filters
+	 *
+	 * @return $this
+	 */
+	public function applyFilters()
+	{
+		if ($this->skipFilter === false) {
 
-    /**
-     * @param array $data
-     * @return mixed
-     */
-    public function create(array $data)
-    {
-        return $this->getModel()->create($data);
-    }
+			$this->pushTracked();
 
-    /**
-     * @param array $data
-     * @param $id
-     * @param string $attribute
-     * @return mixed
-     */
-    public function update(array $data, $id, $attribute = "id")
-    {
-        return $this->getModel()->where($attribute, '=', $id)->update($data);
-    }
+			foreach ($this->getFilters() as $filter) {
+				$this->applyFilter($filter);
+			}
 
-    /**
-     * @param $id
-     * @return mixed
-     */
-    public function delete($id)
-    {
-        return $this->getModel()->where('id', $id)->delete();
-    }
+		}
 
-    /**
-     * @param $id
-     * @param array $columns
-     * @return mixed
-     */
-    public function find($id, $columns = ['*'])
-    {
-        return $this->getModel()->findOrFail($id, $columns);
-    }
+		//dd($this->getFilters());
 
-    public function toHtml()
-    {
-        foreach ($this->getFilters() as $filter) {
-            if ((in_array(get_class($filter), $this->track()) || in_array(get_class($filter), $this->trackedFilters)) && $filter instanceof FormFilter) {
-                $filter->render();
-            }
-        }
-    }
+		return $this;
+	}
 
-    public function canDraw()
-    {
-        foreach ($this->getFilters() as $filter) {
-            if ((in_array(get_class($filter), $this->track()) || in_array(get_class($filter), $this->trackedFilters)) && $filter instanceof FormFilter && $filter->canRender()) {
-                return true;
-            }
-        }
+	/**
+	 * @return $this
+	 */
+	public function pushTracked()
+	{
+		if ($this->isTracked === true) {
+			$filters = array_unique(array_merge($this->track(), $this->trackedFilters));
+			foreach ($filters as $filterClass) {
+				$filter = $this->app->make($filterClass);
+				try {
+					if (!$filter instanceof FilterInterface) {
+						throw new \Exception(trans('repo::exception.instance', ['class' => $filterClass, 'instance' => FilterInterface::class]));
+					}
+				} catch (\Exception $exception) {
+					dd($exception->getMessage());
+				}
 
-        return false;
-    }
+				$this->pushFilter($filter);
+			}
+		}
+
+		return $this;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function track(): array
+	{
+		return [];
+	}
+
+	/**
+	 * @param FilterInterface $filter
+	 *
+	 * @return $this
+	 */
+	public function pushFilter(FilterInterface $filter)
+	{
+		$this->filters->push($filter);
+
+		return $this;
+	}
+
+	/**
+	 * @return Collection
+	 */
+	public function getFilters()
+	{
+		return $this->filters;
+	}
+
+	/**
+	 * @param FilterInterface $filter
+	 *
+	 * @return $this
+	 */
+	public function applyFilter(FilterInterface $filter)
+	{
+		$this->model = $filter->apply($this->model, $this);
+
+		return $this;
+	}
+
+	public function toSql()
+	{
+		return $this->getModel()->toSql();
+	}
+
+	public function getBindings()
+	{
+		return $this->getModel()->getBindings();
+	}
+
+	public function orderBy(array $columns = [])
+	{
+		if (!empty($columns)) {
+
+			foreach ($columns as $column_name => $direction) {
+				if (is_int($column_name)) {
+					$column_name = $direction;
+					$direction = 'ASC';
+				}
+				$this->model = $this->getModel()->orderBy($column_name, $direction);
+			}
+		}
+
+		return $this;
+	}
+
+	/**
+	 * @param $filter
+	 *
+	 * @return $this
+	 */
+	public function pushTrackFilter($filter)
+	{
+		$this->trackedFilters[] = $filter;
+
+		return $this;
+	}
+
+	/**
+	 * @param $offset
+	 * @param $limit
+	 *
+	 * @param array $columns
+	 *
+	 * @return Collection
+	 */
+	public function offset($offset, $limit, $columns = ['*'])
+	{
+		$this->applyFilters();
+
+		return $this->getModel()
+			->select($columns)
+			->offset($offset)
+			->limit($limit)
+			->get();
+	}
+
+	/**
+	 * @param $chunk
+	 * @param callable $callback
+	 *
+	 * @return mixed
+	 */
+	public function chunk($chunk, callable $callback)
+	{
+		$this->applyFilters();
+
+		return $this->getModel()->chunk($chunk, $callback);
+	}
+
+	public function count()
+	{
+		$this->applyFilters();
+
+		return $this->getModel()->count();
+	}
+
+	/**
+	 * @param int $perPage
+	 * @param array $columns
+	 *
+	 * @param string $pageName
+	 * @param null $page
+	 *
+	 * @return mixed
+	 */
+	public function paginate($perPage = null, $columns = ['*'], $pageName = 'page', $page = null)
+	{
+		$this->applyFilters();
+		if (request()->has('sql')) {
+			//dump($this->getModel());
+			dump($this->getModel()->getBindings());
+			dd($this->getModel()->toSql());
+		}
+
+
+		return $this->getModel()
+			->paginate($perPage, $columns, $pageName, $page)
+			->appends(request()->except(['page', '_token']));
+	}
+
+	/**
+	 * @param array $data
+	 *
+	 * @return mixed
+	 */
+	public function create(array $data)
+	{
+		return $this->getModel()->create($data);
+	}
+
+	/**
+	 * @param array $data
+	 * @param $id
+	 * @param string $attribute
+	 *
+	 * @return mixed
+	 */
+	public function update(array $data, $id, $attribute = "id")
+	{
+		return $this->getModel()->where($attribute, '=', $id)->update($data);
+	}
+
+	/**
+	 * @param $id
+	 *
+	 * @return mixed
+	 */
+	public function delete($id)
+	{
+		return $this->getModel()->where('id', $id)->delete();
+	}
+
+	/**
+	 * @param $id
+	 * @param array $columns
+	 *
+	 * @return mixed
+	 */
+	public function find($id, $columns = ['*'])
+	{
+		return $this->getModel()->findOrFail($id, $columns);
+	}
+
+	public function toHtml()
+	{
+		foreach ($this->getFilters() as $filter) {
+			if ((in_array(get_class($filter), $this->track()) || in_array(get_class($filter), $this->trackedFilters)) && $filter instanceof FormFilter) {
+				$filter->render();
+			}
+		}
+	}
+
+	public function canDraw()
+	{
+		foreach ($this->getFilters() as $filter) {
+			if ((in_array(get_class($filter), $this->track()) || in_array(get_class($filter), $this->trackedFilters)) && $filter instanceof FormFilter && $filter->canRender()) {
+				return true;
+			}
+		}
+
+		return false;
+	}
 
 }
